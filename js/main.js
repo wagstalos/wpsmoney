a32a078b0be16aabd1c731820317993be8c05ef8
Wagner Paulo <wagstalos@hotmail.com>
2020-05-26 16:31:37 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
+    
+    $('.icon').click(function () {
+        $('.icon').toggleClass('active');
+    })
+});
+
+
+
+
